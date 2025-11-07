cf674e7b8abac6dc57734a3ef03d6fd48536a627
lae-delacruz <laetishadanielle.delacruz.eng@ust.edu.ph>
2025-11-07 21:38:14 +0800

    Add files via upload

---
+++ isolateloop.m
@@ -0,0 +1,20 @@
+% PA 6 Problem 3 (Isolated loop)
+
+%Create function
+function isolateloop(x)
+
+% Empty vectors to store even and odd numbers separately
+even = [];
+odd = [];
+
+% Loop to classify odd from even
+for n = 1:length(x)
+    if mod(x(n), 2) == 0
+       even = [even, x(n)]; % If even, store to even vector
+      
+    else
+        odd = [odd, x(n)]; % If odd store to odd vector
+    end
+end
+
+ 
